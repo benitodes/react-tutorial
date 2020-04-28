469d6e78482b8cfee40e153acc371cfd198325eb
benoit destresse <benoit.destresse@gmail.com>
2020-04-28 15:54:37 +0800

    syntax corrections and refactoring

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React, { Component } from 'react'
-import Table from './Table'
+import React, { Component } from 'react';
+import Table from './Table';
 import Form from './Form';
 
 class App extends Component {
+    state = {
+        characters: []
+    };
 
-  state = {
-    characters: [],
-  }
+    removeCharacter = index => {
+        const { characters } = this.state;
 
-  render() {
-    const { characters } = this.state
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
 
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-        <Form />
-      </div>
-    )
-  }
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
 
-  removeCharacter = index => {
-    const { characters } = this.state
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
+    render() {
+        const { characters } = this.state;
+
+        return (
+            <div className="container">
+                <h1>React Tutorial</h1>
+                <p>Add a character with a name and a job to the table.</p>
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <h3>Add New</h3>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 
 class Form extends Component {
-  initialState = {
-    name: '',
-    job: '',
-  }
+    constructor(props) {
+        super(props);
 
-  state = this.initialState
+        this.initialState = {
+            name: '',
+            job: ''
+        };
 
-handleChange = event => {
-  const { name, value } = event.target
+        this.state = this.initialState;
+    }
 
-  this.setState({
-    [name]: value,
-  })
-}
+    handleChange = event => {
+        const { name, value } = event.target;
 
-render() {
-  const { name, job } = this.state;
+        this.setState({
+            [name] : value
+        });
+    }
 
-  return (
-    <form>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        value={name}
-        onChange={this.handleChange} />
-      <label htmlFor="job">Job</label>
-      <input
-        type="text"
-        name="job"
-        id="job"
-        value={job}
-        onChange={this.handleChange} />
-    </form>
-  );
-}
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.handleSubmit(this.state);
+        this.setState(this.initialState);
+    }
+
+    render() {
+        const { name, job } = this.state;
+
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <label for="name">Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={this.handleChange} />
+                <label for="job">Job</label>
+                <input
+                    type="text"
+                    name="job"
+                    id="job"
+                    value={job}
+                    onChange={this.handleChange} />
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        );
+    }
 }
 
 export default Form;
