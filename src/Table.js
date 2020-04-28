e7bdddc8d5d7c1381030cdac7de32224f6fc1283
benoit destresse <benoit.destresse@gmail.com>
2020-04-28 10:19:58 +0800

    created Table.js

---
+++ src/Table.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+          </tr>
+        </thead>
+        <tboby>
+          <td>
+            <tr>Charlie</tr>
+            <tr>Janitor</tr>
+          </td>
+          <td>
+            <tr>Mac</tr>
+            <tr>Bouncer</tr>
+          </td>
+          <td>
+            <tr>Dee</tr>
+            <tr>Aspiring actress</tr>
+          </td>
+          <td>
+            <tr>Dennis</tr>
+            <tr>Bartender</tr>
+          </td>
+        </tboby>
+      </table>
+    )
+  }
+}
+
+export default Table
