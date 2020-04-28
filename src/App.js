415fefe0f8bd95ef086aa4d442d63674c6b34845
benoit destresse <benoit.destresse@gmail.com>
2020-04-28 15:29:06 +0800

    remove data from state in App.js

--- src/App.js
+++ src/App.js
@@ -4,24 +4,7 @@ import Table from './Table'
 class App extends Component {
 
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
   render() {
